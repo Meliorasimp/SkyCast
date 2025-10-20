2322a2876eda41a8edc290111eaf331b0a67844a
Meinard Legashki Limlengco <meliorasimp@gmail.com>
2025-10-21 00:34:32 +0800

    Refactored landingpage Code

--- client/src/pages/Landingpage.tsx
+++ client/src/pages/Landingpage.tsx
@@ -8,6 +8,50 @@ import Destination from "../assets/destination.png";
 import MarineWeather from "../assets/MarineWeather.png";
 import Warning from "../assets/warning.png";
 const Landingpage = () => {
+  const WeatherInfoData = [
+    {
+      name: "Real-time Forecast",
+      description:
+        "Get the latest weather updates and forecasts using Trusted APIs",
+      icon: Meterology,
+      alticon: "Meteorology Icon",
+    },
+    {
+      name: "Air Quality",
+      description:
+        "Monitor air pollution levels to protect your health and well-being",
+      icon: Wind,
+      alticon: "Wind Icon",
+    },
+    {
+      name: "Astronomy",
+      description:
+        "Explore celestial events and moon phases for stargazing enthusiasts",
+      icon: Moon,
+      alticon: "Moon Icon",
+    },
+    {
+      name: "Geolocation",
+      description:
+        "Access your location-based weather data for accurate forecasts",
+      icon: Destination,
+      alticon: "Destination Icon",
+    },
+    {
+      name: "Marine Weather",
+      description:
+        "Get specialized weather forecasts for marine and coastal areas",
+      icon: MarineWeather,
+      alticon: "Marine Weather Icon",
+    },
+    {
+      name: "Alerts",
+      description:
+        "Stay informed about severe weather alerts and warnings in your area",
+      icon: Warning,
+      alticon: "Warning Icon",
+    },
+  ];
   return (
     <div className="landing-page-gradient-bg min-h-screen relative">
       <Stars />
@@ -31,66 +75,18 @@ const Landingpage = () => {
           All the weather info you need in one place
         </h1>
         <section className="w-5/6 flex flex-wrap items-center justify-between h-4/5 gap-y-8">
-          <div className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center">
-            <div className="flex justify-between flex-col items-center gap-y-4">
-              <img src={Meterology} alt="Meteorology" className="w-16 h-16" />
-              <h1 className="text-xl font-bold">Real-time Forecast</h1>
+          {WeatherInfoData.map((info, index) => (
+            <div
+              key={index}
+              className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center"
+            >
+              <div className="flex justify-between flex-col items-center gap-y-4">
+                <img src={info.icon} alt={info.alticon} className="w-16 h-16" />
+                <h1 className="text-xl font-bold">{info.name}</h1>
+              </div>
+              <p className="text-center w-5/6">{info.description}</p>
             </div>
-            <p className="text-center w-5/6">
-              Get the latest weather updates and forecasts using Trusted APIs
-            </p>
-          </div>
-          <div className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center">
-            <div className="flex justify-between flex-col items-center gap-y-4">
-              <img src={Wind} alt="Wind" className="w-16 h-16" />
-              <h1 className="text-xl font-bold">Air Quality</h1>
-            </div>
-            <p className="text-center w-5/6">
-              Monitor air pollution levels to protect your health and well-being
-            </p>
-          </div>
-          <div className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center">
-            <div className="flex justify-between flex-col items-center gap-y-4">
-              <img src={Moon} alt="Wind" className="w-16 h-16" />
-              <h1 className="text-xl font-bold">Astronomy</h1>
-            </div>
-            <p className="text-center w-5/6">
-              Explore celestial events and moon phases for stargazing
-              enthusiasts
-            </p>
-          </div>
-          <div className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center">
-            <div className="flex justify-between flex-col items-center gap-y-4">
-              <img src={Destination} alt="Destination" className="w-16 h-16" />
-              <h1 className="text-xl font-bold">Geolocation</h1>
-            </div>
-            <p className="text-center w-5/6">
-              Access your location-based weather data for accurate forecasts
-            </p>
-          </div>
-          <div className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center">
-            <div className="flex justify-between flex-col items-center gap-y-4">
-              <img
-                src={MarineWeather}
-                alt="Destination"
-                className="w-16 h-16"
-              />
-              <h1 className="text-xl font-bold">Marine Weather</h1>
-            </div>
-            <p className="text-center w-5/6">
-              Get specialized weather forecasts for marine and coastal areas
-            </p>
-          </div>
-          <div className="w-2/7 h-1/2 rounded-2xl glass-morphism-animated flex flex-col gap-y-4 justify-center items-center">
-            <div className="flex justify-between flex-col items-center gap-y-4">
-              <img src={Warning} alt="Destination" className="w-16 h-16" />
-              <h1 className="text-xl font-bold">Alerts</h1>
-            </div>
-            <p className="text-center w-5/6">
-              Stay informed about severe weather alerts and warnings in your
-              area
-            </p>
-          </div>
+          ))}
         </section>
       </div>
     </div>
